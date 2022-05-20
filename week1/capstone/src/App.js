bc7d6ec1a983ecf1f0f7e4bc48136176a38be6d5
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2022-05-20 12:54:54 -0400

    1st

---
+++ week1/capstone/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <h1>My New Pizza App</h1>
+    </div>
+  );
+}
+
+export default App;
